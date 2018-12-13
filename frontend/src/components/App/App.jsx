77cbe5e9d04b7eb18afd8bf69ce3298f329eb5f3
sun1211 <ngoctam121191@yahoo.com>
2018-12-13 15:59:50 +0700

    show Referral as pop up layout

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -7,39 +7,68 @@ import { Header, BetTable, PlayMenu, Referral } from 'components';
 import DiceBoard from 'components/DiceBoard/DiceBoard.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Referral: false,
+    }
+
+    this.handleReferralAction = this.handleReferralAction.bind(this);
+    // this.handleReferral = this.handleReferral.bind(this);
+  }
+
+  handleReferralAction() {
+
+    console.log('tam_ handle referral');
+    this.setState({ Referral: !this.state.Referral })
+
+
+  }
 
   render() {
     const dice = {
       countDown: true,
       time2dice: 1,
-      numberOfDices:3,
+      numberOfDices: 3,
 
-      dice1timer:"4s",
+      dice1timer: "4s",
       dice1value: 1,
 
-      dice2timer:"3s",
+      dice2timer: "3s",
       dice2value: 3,
 
-      dice3timer:"2s",
+      dice3timer: "2s",
       dice3value: 4
     };
 
     return (
       <div className="App">
-        <Header />
-        {/* <div className="dice_board">
+        <Header {...this.props} handleReferral={this.handleReferralAction} />
+        {/* <Header {...this.props} /> */}
 
-        <div className="dice_board_center">
-            <DiceBoard />
+        {
+          this.state.Referral ?
+          <div className="play_menu">
+            <Referral />
+          </div>
+          : 
+          <div >
+          <div className="dice_board">
+
+            <div className="dice_board_center">
+              <DiceBoard />
+            </div>
+
+          </div>
+          <div className="dice_table">
+            <BetTable />
+          </div>
+
         </div>
+        }
 
-        </div>        
-        <div className="dice_table">
-          <BetTable />
-        </div> */}
-        <div className="play_menu">
-          <Referral/>
-        </div>        
+
+
 
       </div>
     );
