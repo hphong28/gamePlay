137d09f592ef013220940f87969489d06cadc83c
quoc le <quocle@orgos.net>
2018-12-14 08:00:34 +0700

    remove old rolling module

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,7 +1,6 @@
 import App from './App';
 import Header from './Header'
 import BetTable from './BetTable'
-import RollingDice from './RollingDice'
 import PlayMenu from './PlayMenu'
 
 
@@ -9,6 +8,5 @@ export {
   App,
   Header,
   BetTable,
-  RollingDice,
   PlayMenu,
 }
