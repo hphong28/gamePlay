eaa8f02a710c4ef6aad3bc6838c39c8145af2240
sun1211 <ngoctam121191@yahoo.com>
2019-01-08 14:50:15 +0700

    add full token list layout

--- frontend/src/components/PlayMenu/PlayMenu.jsx
+++ frontend/src/components/PlayMenu/PlayMenu.jsx
@@ -18,18 +18,18 @@ class PlayMenu extends Component {
     this.handleChoseToken = this.handleChoseToken.bind(this);
 
     this.state = {
-        chip: 0,
-        token: 1,
+      chip: 0,
+      token: 1,
     };
   }
   handleClick = (value) => (e) => {
-    this.setState({ chip: value == this.state.chip ? 0 :value });
-    this.props.onSelectedChip(value == this.state.chip ? 0 :value );
-  }; 
-  
+    this.setState({ chip: value == this.state.chip ? 0 : value });
+    this.props.onSelectedChip(value == this.state.chip ? 0 : value);
+  };
+
   handleChoseToken = (value) => (e) => {
     console.log('tam_ handleChoseToken', value);
-    this.setState({ token: value})
+    this.setState({ token: value })
   }
 
 
@@ -41,43 +41,48 @@ class PlayMenu extends Component {
 
         <div className="WrapIconBet">
           <ul>
-            <div onClick={this.handleClick(0.5)} className={`iconBet ${this.state.chip  == 0.5 ? "active" : ""}`} ><img src={bet0_5} alt=" " /></div>
-            <div onClick={this.handleClick(1)}  className={`iconBet ${this.state.chip  == 1 ? "active" : ""}`} ><img src={bet1} alt=" "/></div>
-            <div onClick={this.handleClick(2)} className={`iconBet ${this.state.chip  == 2 ? "active" : ""}`} ><img src={bet2} alt=" " /></div>
-            <div onClick={this.handleClick(5)} className={`iconBet ${this.state.chip  == 5 ? "active" : ""}`} ><img src={bet5} alt=" " /></div>
-            <div onClick={this.handleClick(10)} className={`iconBet ${this.state.chip  == 10 ? "active" : ""}`} ><img src={bet10} alt=" " /></div>
+            <div onClick={this.handleClick(0.5)} className={`iconBet ${this.state.chip == 0.5 ? "active" : ""}`} ><img src={bet0_5} alt=" " /></div>
+            <div onClick={this.handleClick(1)} className={`iconBet ${this.state.chip == 1 ? "active" : ""}`} ><img src={bet1} alt=" " /></div>
+            <div onClick={this.handleClick(2)} className={`iconBet ${this.state.chip == 2 ? "active" : ""}`} ><img src={bet2} alt=" " /></div>
+            <div onClick={this.handleClick(5)} className={`iconBet ${this.state.chip == 5 ? "active" : ""}`} ><img src={bet5} alt=" " /></div>
+            <div onClick={this.handleClick(10)} className={`iconBet ${this.state.chip == 10 ? "active" : ""}`} ><img src={bet10} alt=" " /></div>
           </ul>
         </div>
 
         <div className="BetAmount_Wrap" >
-        <div onClick={this.handleChoseToken(0)}>
-          <div class="menu_playmenu icon_playmenu"></div>
-        </div>
-        
 
-        <div className="Dropdown_Wrap">
+          <div className="MenuSelect_Wrap">
 
-          <div className={`BetAmount${(this.state.token  == 1) || (this.state.token  == 0) ? "Show" : "Hidden"}`} onClick={this.handleChoseToken(1)} >
-            <img src={LogoBetAmount} alt=" " className="BetAmountLogo" />
-            <div className="BetAmountValue"> 5.0</div>
-            <div className="BetAmountToken"> EOS</div>
+            <div className="icon_wrap" onClick={this.handleChoseToken(0)}>
+              <div class="menu_playmenu icon_playmenu"></div>
+            </div>
+
+
+            <div className="Dropdown_Wrap">
+
+              <div className={`BetAmount${(this.state.token == 1) || (this.state.token == 0) ? "Show" : "Hidden"}`} onClick={this.handleChoseToken(1)} >
+                <img src={LogoBetAmount} alt=" " className="BetAmountLogo" />
+                <div className="BetAmountValue"> 5.0</div>
+                <div className="BetAmountToken"> EOS</div>
+              </div>
+
+              <div className={`BetAmount${(this.state.token == 2) || (this.state.token == 0) ? "Show" : "Hidden"}`} onClick={this.handleChoseToken(2)}>
+                <img src={LogoBetAmount} alt=" " className="BetAmountLogo" />
+                <div className="BetAmountValue"> 4</div>
+                <div className="BetAmountToken"> EOS</div>
+              </div>
+
+              <div className={`BetAmount${(this.state.token == 3) || (this.state.token == 0) ? "Show" : "Hidden"}`} onClick={this.handleChoseToken(3)}>
+                <img src={LogoBetAmount} alt=" " className="BetAmountLogo" />
+                <div className="BetAmountValue"> 3</div>
+                <div className="BetAmountToken"> EOS</div>
+              </div>
+
+            </div>
+
           </div>
 
-          <div className={`BetAmount${(this.state.token  == 2) || (this.state.token  == 0)  ? "Show" : "Hidden"}`} onClick={this.handleChoseToken(2)}>
-            <img src={LogoBetAmount} alt=" " className="BetAmountLogo" />
-            <div className="BetAmountValue"> 4</div>
-            <div className="BetAmountToken"> EOS</div>
-          </div>
 
-          <div className={`BetAmount${(this.state.token  == 3) || (this.state.token  == 0)  ? "Show" : "Hidden"}`} onClick={this.handleChoseToken(3)}>
-            <img src={LogoBetAmount} alt=" " className="BetAmountLogo" />
-            <div className="BetAmountValue"> 3</div>
-            <div className="BetAmountToken"> EOS</div>
-          </div>
-
-          </div>
-
-          
 
 
 
