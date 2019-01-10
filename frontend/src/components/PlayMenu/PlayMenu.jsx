6301cafe59e72f52f660133f853f5c8fb277af2d
sun1211 <ngoctam121191@yahoo.com>
2019-01-10 09:28:26 +0700

    update token value

--- frontend/src/components/PlayMenu/PlayMenu.jsx
+++ frontend/src/components/PlayMenu/PlayMenu.jsx
@@ -16,11 +16,12 @@ class PlayMenu extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.handleChoseToken = this.handleChoseToken.bind(this);
+    this.handleChosetokenSelect = this.handleChosetokenSelect.bind(this);
 
     this.state = {
       chip: 0,
-      token: 1,
+      tokenSelect: 1,
+      tokenValue: 0,
       net: 0,
       CPU: 0,
 
@@ -32,9 +33,10 @@ class PlayMenu extends Component {
     ApiService.hasIdentity().then(rsp => {
 			if (rsp) {
         ApiService.GetAccountDetail(rsp.accounts[0].name).then(rawData => {
-          // console.log('tam_ playmenu rawData', rawData, rawData.net_limit.available);
+          console.log('tam_ playmenu rawData', rawData, rawData.net_limit.available);
 
           this.setState({
+            tokenValue: parseFloat(rawData.core_liquid_balance).toFixed(0),
           	net: (rawData.net_limit.used /  rawData.net_limit.available * 100).toFixed(0),
           	CPU: (rawData.cpu_limit.used /  rawData.cpu_limit.available * 100).toFixed(0),
           });
@@ -55,9 +57,9 @@ class PlayMenu extends Component {
     this.props.onSelectedChip(value == this.state.chip ? 0 : value);
   };
 
-  handleChoseToken = (value) => (e) => {
-    console.log('tam_ handleChoseToken', value);
-    this.setState({ token: value })
+  handleChosetokenSelect = (value) => (e) => {
+    console.log('tam_ handleChosetokenSelect', value);
+    this.setState({ tokenSelect: value })
   }
 
 
@@ -81,25 +83,25 @@ class PlayMenu extends Component {
 
           <div className="MenuSelect_Wrap">
 
-            <button className="showButton_wrap"  onClick={this.handleChoseToken(0)}>
+            <button className="showButton_wrap"  onClick={this.handleChosetokenSelect(0)}>
                 <div class="menu_playmenu icon_playmenu"></div>
             </button>
 
             <div className="Dropdown_Wrap">
 
-              <div className={`BetAmount${(this.state.token == 1) || (this.state.token == 0) ? "Show" : "Hidden"}`} onClick={this.handleChoseToken(1)} >
+              <div className={`BetAmount${(this.state.tokenSelect == 1) || (this.state.tokenSelect == 0) ? "Show" : "Hidden"}`} onClick={this.handleChosetokenSelect(1)} >
                 <img src={LogoBetAmount} alt=" " className="BetAmountLogo" />
-                <div className="BetAmountValue"> 5.0</div>
+                <div className="BetAmountValue">{this.state.tokenValue}</div>
                 <div className="BetAmountToken"> EOS</div>
               </div>
 
-              <div className={`BetAmount${(this.state.token == 2) || (this.state.token == 0) ? "Show" : "Hidden"}`} onClick={this.handleChoseToken(2)}>
+              <div className={`BetAmount${(this.state.tokenSelect == 2) || (this.state.tokenSelect == 0) ? "Show" : "Hidden"}`} onClick={this.handleChosetokenSelect(2)}>
                 <img src={LogoBetAmount} alt=" " className="BetAmountLogo" />
                 <div className="BetAmountValue"> 4</div>
                 <div className="BetAmountToken"> EOS</div>
               </div>
 
-              <div className={`BetAmount${(this.state.token == 3) || (this.state.token == 0) ? "Show" : "Hidden"}`} onClick={this.handleChoseToken(3)}>
+              <div className={`BetAmount${(this.state.tokenSelect == 3) || (this.state.tokenSelect == 0) ? "Show" : "Hidden"}`} onClick={this.handleChosetokenSelect(3)}>
                 <img src={LogoBetAmount} alt=" " className="BetAmountLogo" />
                 <div className="BetAmountValue"> 3</div>
                 <div className="BetAmountToken"> EOS</div>
